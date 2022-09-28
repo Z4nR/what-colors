ab1095e7ada529f61cf8ceedcc3e9120f5562a59
Zulham Ari <ari.comunity1@gmail.com>
2022-09-29 01:18:14 +0700

    set getLcoalItem

--- src/components/test/TestSheet.js
+++ src/components/test/TestSheet.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 
 export default function TestSheet() {
-  const localData = useState(localStorage.setItem("data") || null);
+  const localData = useState(localStorage.getItem("data") || null);
+  console.log(localData);
 
   return <p>Hello</p>;
 }
