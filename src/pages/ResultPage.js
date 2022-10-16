9f74dd464ea48bd340c86c549784df5ac588a1f3
Zulham Ari <ari.comunity1@gmail.com>
2022-10-16 08:30:50 +0700

    fixed delay displaying chart

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -16,67 +16,64 @@ export default function ResultPage() {
     setDiscriminant(JSON.parse(discriminantResult));
   }, []);
 
-  const label = getDiscriminant?.number;
-  const data = getDiscriminant?.result;
-
-  console.log(label);
-  console.log(data);
-
-  const chartData = {
-    labels: label,
-    datasets: [
-      {
-        label: "Hue Discriminant",
-        data: data,
-        fill: true,
-        backgroundColor: "rgba(255, 99, 132, 0.2)",
-        borderColor: "rgb(255, 99, 132)",
-        pointBackgroundColor: "rgb(255, 99, 132)",
-        pointBorderColor: "#fff",
-        pointHoverBackgroundColor: "#fff",
-        pointHoverBorderColor: "rgb(255, 99, 132)",
-      },
-    ],
-  };
-
-  const config = {
-    type: "radar",
-    data: chartData,
-    options: {
-      elements: {
-        line: {
-          borderWidth: 3,
-        },
-      },
-      scales: {
-        r: {
-          pointLabels: {
-            color: "red",
-          },
-          suggestedMin: 0,
-          suggestedMax: 20,
-          ticks: {
-            color: "red",
-            stepSize: 2,
-          },
-        },
-      },
-      spanGaps: true,
-      plugins: {
-        title: {
-          display: true,
-          text: "Discriminant Value Test Result",
-        },
-      },
-    },
-  };
-
   useEffect(() => {
+    const label = getDiscriminant?.number;
+    const data = getDiscriminant?.result;
+
+    const chartData = {
+      labels: label,
+      datasets: [
+        {
+          label: "Hue Discriminant",
+          data: data,
+          fill: true,
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          borderColor: "rgb(255, 99, 132)",
+          pointBackgroundColor: "rgb(255, 99, 132)",
+          pointBorderColor: "#fff",
+          pointHoverBackgroundColor: "#fff",
+          pointHoverBorderColor: "rgb(255, 99, 132)",
+        },
+      ],
+    };
+
+    const config = {
+      type: "radar",
+      data: chartData,
+      options: {
+        elements: {
+          line: {
+            borderWidth: 3,
+          },
+        },
+        scales: {
+          r: {
+            pointLabels: {
+              color: "red",
+            },
+            suggestedMin: 0,
+            suggestedMax: 20,
+            ticks: {
+              color: "red",
+              stepSize: 2,
+            },
+          },
+        },
+        spanGaps: true,
+        plugins: {
+          title: {
+            display: true,
+            text: "Discriminant Value Test Result",
+          },
+        },
+      },
+    };
+
     if (getDiscriminant !== null) {
       const radar = new Chart("radar-chart", config);
       console.log(radar);
     }
-  }, []);
+  }, [getDiscriminant]);
 
   return (
     <section>
