a2b9162faed557b7ff4b77dff13f5f7f4f8bc61c
Zulham Ari <ari.comunity1@gmail.com>
2022-11-11 05:26:01 +0700

    fnisihing thanks page

--- src/pages/utils/ThanksPage.js
+++ src/pages/utils/ThanksPage.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Thanks() {
   return (
     <section>
-      <p>Thanks</p>
+      <div className="util-box image-thanks">
+        <h1>Thanks for doing room test</h1>
+        <h4>
+          Doing other test?{" "}
+          <span>
+            <Link to="/test">Back to Test Page</Link>
+          </span>
+        </h4>
+      </div>
     </section>
   );
 }
