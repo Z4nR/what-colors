fef9af15ccbb32d6464697ab4e1c88e71b3f7a52
Zulham Ari <ari.comunity1@gmail.com>
2022-12-02 13:50:20 +0700

    styling article finish

--- src/components/article/ArticleBody.js
+++ src/components/article/ArticleBody.js
@@ -10,14 +10,17 @@ export default function ArticleBody({
 }) {
   return (
     <div className="article-container">
-      <h3>
-        <a href={url}>{title}</a>
-      </h3>
-      <h4>
-        Penulis : {author} ({year})
-      </h4>
-      <h4>Kategori : {category}</h4>
-      <p className="article-desc">Deskripsi : {description}</p>
+      <div className="article-item">
+        <h3>
+          <a href={url}>{title}</a>
+        </h3>
+        <h4>
+          Penulis : {author} ({year})
+        </h4>
+        <h4>Kategori : {category}</h4>
+        <p>Deskripsi : {description}</p>
+      </div>
+      <input className="expand-btn" type="checkbox" />
     </div>
   );
 }
