ce6324f70252fede60b0f9a06b73fa0022fd6f6a
Zulham Ari <ari.comunity1@gmail.com>
2022-12-04 23:43:09 +0700

    styling result page

---
+++ src/components/result/ResultData.js
@@ -0,0 +1,168 @@
+import React, { useEffect, useMemo, useState } from "react";
+import Chart from "chart.js/auto";
+import { getUserData } from "../../utils/data-api";
+import { useMediaQuery } from "react-responsive";
+import LoadingPage from "../../pages/utils/LoadingPage";
+
+export default function ResultData() {
+  const [result, setResult] = useState(null);
+  const [getDiscriminant, setDiscriminant] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+  const isDesktop = useMediaQuery({
+    query: "(min-width: 450px)",
+  });
+
+  useEffect(() => {
+    const discriminantResult = localStorage.getItem("discriminantResult");
+    setDiscriminant(JSON.parse(discriminantResult));
+
+    const id = localStorage.getItem("id");
+
+    getUserData(id).then((data) => {
+      setLoading(false);
+      setResult(data.data);
+    });
+  }, []);
+
+  const maxResult = useMemo(() => {
+    if (getDiscriminant !== null) {
+      Math.max(...getDiscriminant?.result);
+    }
+  }, [getDiscriminant]);
+
+  useEffect(() => {
+    const label = getDiscriminant?.number;
+    const data = getDiscriminant?.result;
+
+    const chartData = {
+      labels: label,
+      datasets: [
+        {
+          label: "Hue Discriminant",
+          data: data,
+          fill: true,
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          borderColor: "rgb(255, 99, 132)",
+          pointBackgroundColor: "rgb(255, 99, 132)",
+          pointBorderColor: "#fff",
+          pointHoverBackgroundColor: "#fff",
+          pointHoverBorderColor: "rgb(255, 99, 132)",
+        },
+      ],
+    };
+
+    const config = {
+      type: "radar",
+      data: chartData,
+      options: {
+        elements: {
+          line: {
+            borderWidth: 3,
+          },
+        },
+        scales: {
+          r: {
+            pointLabels: {
+              color: "black",
+            },
+            suggestedMin: 0,
+            suggestedMax: maxResult,
+            ticks: {
+              color: "blue",
+              stepSize: 2,
+            },
+          },
+        },
+        spanGaps: true,
+      },
+    };
+
+    if (getDiscriminant !== null) {
+      if (isDesktop) {
+        if (isLoading === false) {
+          new Chart("radar-chart", config);
+        }
+      }
+    }
+  }, [getDiscriminant, maxResult, isDesktop, isLoading]);
+
+  return isLoading === false ? (
+    <div className="result">
+      <div className="data-box">
+        <div className="result-box">
+          <div className="info-result">
+            <h3>Hasil Komparasi : </h3>
+            <p>
+              Hasil komparasi diambil dari hasil tes yang kamu lakukan kemudian
+              dibandingkan dengan nilai asli
+            </p>
+            <div className="table-data">
+              <table>
+                <thead>
+                  <tr>
+                    <th>No.</th>
+                    <th>Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {result?.comparisonResults?.map((cap) => (
+                    <tr className="cap-data" key={cap._id}>
+                      <td>{cap._id}</td>
+                      <td>{cap.comparison}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="chart-box">
+        <div className="chart-card">
+          {isDesktop ? (
+            <div>
+              <h3>Hasil Diskirminasi</h3>
+              <p>
+                Hasil komparasi diambil dari hasil tes yang kamu lakukan
+                kemudian dibandingkan dengan nilai asli
+              </p>
+              <canvas id="radar-chart" />
+            </div>
+          ) : (
+            <div className="info-result">
+              <h3>Discriminant Result : </h3>
+              <p>
+                Hasil komparasi diambil dari hasil tes yang kamu lakukan
+                kemudian dibandingkan dengan nilai asli
+              </p>
+              <div className="table-data">
+                <table>
+                  <thead>
+                    <tr>
+                      <th>No.</th>
+                      <th>Value</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {result?.discriminantResults?.map((cap) => (
+                      <tr className="cap-data" key={cap._id}>
+                        <td>{cap._id}</td>
+                        <td>{cap.discriminant}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="util-box">
+      <LoadingPage />
+      <p>Harap Tunggu</p>
+    </div>
+  );
+}
