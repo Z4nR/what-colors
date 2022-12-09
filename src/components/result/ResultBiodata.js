83a1d997d59ae11ade314e68b82aaf3d676af765
Zulham Ari <ari.comunity1@gmail.com>
2022-12-09 22:58:28 +0700

    Result Biodata done data

--- src/components/result/ResultBiodata.js
+++ src/components/result/ResultBiodata.js
@@ -4,16 +4,32 @@ export default function ResultBiodata({ data }) {
   if (data?.errorScoreStatus === "Superior") {
     return (
       <div className="result">
-        <div className="biodata_item" style={{ backgroundColor: "#f0e68c4d" }}>
-          <h3>Halllo {data?.fullName}</h3>
+        <div className="biodata_item" style={{ backgroundColor: "#91ee914d" }}>
+          <h3>
+            {data?.fullName} hasilmu diluar ekspektasi kami, kamu lebih peka
+            dalam bedain warna
+          </h3>
+          <h2>Pertahankan kemampuan matamu</h2>
+          <h3>
+            Dibawah ini nih hasil tes kamu dan jangan lupa jaga terus kesehatan
+            matamu, OKE!!
+          </h3>
         </div>
       </div>
     );
   } else if (data?.errorScoreStatus === "Average") {
     return (
       <div className="result">
-        <div className="biodata_item" style={{ backgroundColor: "#91ee914d" }}>
-          <h3>Halllo {data?.fullName}</h3>
+        <div className="biodata_item" style={{ backgroundColor: "#f0e68c5a" }}>
+          <h3>
+            {data?.fullName} hasilmu merupakan kemampuan rata-rata manusia dalam
+            membedakan warna
+          </h3>
+          <h2>Matamu Normal</h2>
+          <h3>
+            Dibawah ini nih hasil tes kamu dan jangan lupa jaga terus kesehatan
+            matamu, OKE!!
+          </h3>
         </div>
       </div>
     );
