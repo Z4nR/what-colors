0db09577f397887268a373dfb8c7844d5dcf61f3
Zulham Ari <ari.comunity1@gmail.com>
2022-12-09 20:13:13 +0700

    styling about responsive

--- src/components/result/ResultBiodata.js
+++ src/components/result/ResultBiodata.js
@@ -1,11 +1,6 @@
 import React from "react";
-import { useMediaQuery } from "react-responsive";
 
 export default function ResultBiodata({ data }) {
-  const isDesktop = useMediaQuery({
-    query: "(min-width: 450px)",
-  });
-
   return (
     <div className="result">
       <div className="biodata_item">
