5edf3d2da01bca57c6b0e914d91addad5ce63eeb
Zulham Ari <ari.comunity1@gmail.com>
2022-12-13 22:52:04 +0700

    break the homepage

---
+++ src/components/home/GoToTestBtn.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function GoToTestButton(params) {
+  const navigate = useNavigate();
+
+  return (
+    <div className="go-to-box">
+      <div className="go-to-test">
+        <h2>Apakah kamu buta warna? Cek sekarang</h2>
+        <button
+          onClick={(event) => {
+            event.preventDefault();
+            navigate("/test");
+          }}
+        >
+          Coba Tes
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,24 +1,10 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import GoToTestButton from "../components/home/GoToTestBtn";
 
 export default function HomePage() {
-  const navigate = useNavigate();
-
   return (
     <section>
-      <div className="go-to-box">
-        <div className="go-to-test">
-          <h2>Apakah kamu buta warna? Cek sekarang</h2>
-          <button
-            onClick={(event) => {
-              event.preventDefault();
-              navigate("/test");
-            }}
-          >
-            Coba Tes
-          </button>
-        </div>
-      </div>
+      <GoToTestButton />
     </section>
   );
 }
