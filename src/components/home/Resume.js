dc0b65382bf72b0802602eeda0cd4d772fcfaca8
Zulham Ari <ari.comunity1@gmail.com>
2023-01-07 08:41:41 +0700

    design landing page

--- src/components/home/Resume.js
+++ src/components/home/Resume.js
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function Resume() {
-  return <div className="resume-body"></div>;
+  return (
+    <div className="resume-body">
+      <div className="resume-section">
+        <div className="resume-text">
+          <h1>Info</h1>
+        </div>
+        <div className="resume-image">
+          <img
+            className="image-section"
+            src="https://images.newscientist.com/wp-content/uploads/2018/03/14102838/colourblind011.jpg"
+            alt="Kind of Color Blind"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
