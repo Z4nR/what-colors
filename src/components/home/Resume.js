5544670a9fd114fc1c4f297c7388e3b1c180b192
Zulham Ari <ari.comunity1@gmail.com>
2023-01-09 23:10:58 +0700

    odd section

--- src/components/home/Resume.js
+++ src/components/home/Resume.js
@@ -1,10 +1,12 @@
 import React from "react";
 import SectionOne from "./resume/resume-section-one";
+import SectionThree from "./resume/resume-section-three";
 
 export default function Resume() {
   return (
     <div className="resume-body">
       <SectionOne />
+      <SectionThree />
     </div>
   );
 }
--- src/components/home/resume/resume-section-three.js
+++ src/components/home/resume/resume-section-three.js
@@ -1,17 +1,13 @@
 import React from "react";
 
-export default function SectionOne() {
+export default function SectionThree() {
   return (
     <div className="resume-section odd-section">
       <div className="resume-text">
         <h1>Info</h1>
       </div>
       <div className="resume-image">
-        <img
-          className="image-section"
-          src={require("../../../asset/img/colourblind.webp")}
-          alt="Kind of Color Blind"
-        />
+        <div className="image-section cap-img" />
       </div>
     </div>
   );
