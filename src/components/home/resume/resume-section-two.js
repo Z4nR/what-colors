7e7e8b0ddfe546e0b0cfa870b2b2ab8e72064286
Zulham Ari <ari.comunity1@gmail.com>
2023-01-11 01:58:41 +0700

    setting resume responsive

--- src/components/home/resume/resume-section-two.js
+++ src/components/home/resume/resume-section-two.js
@@ -3,7 +3,14 @@ import React from "react";
 export default function SectionTwo() {
   return (
     <div className="resume-section even-section">
-      <div className="resume-text">
+      <div className="resume-image">
+        <img
+          className="image-section"
+          src={require("../../../asset/img/colourblind.webp")}
+          alt="Kind of Color Blind"
+        />
+      </div>
+      <div className="resume-text even-text">
         <h1>
           Coba lihat gambar disamping, mana yang <b>Warna Asli?</b>
         </h1>
@@ -11,13 +18,6 @@ export default function SectionTwo() {
           Apabila kamu mengalami kebingungan maka kamu menderita{" "}
           <b>buta warna</b>
         </h4>
-      </div>
-      <div className="resume-image">
-        <img
-          className="image-section"
-          src={require("../../../asset/img/colourblind.webp")}
-          alt="Kind of Color Blind"
-        />
       </div>
     </div>
   );
